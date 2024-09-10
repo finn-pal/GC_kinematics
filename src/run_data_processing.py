566422e22133058ae6188cad4c5bc55e501f3c53
finn-pal <173330723+finn-pal@users.noreply.github.com>
2024-09-10 14:00:47 +1000

    Create 3 datasets for analysis

--- src/run_data_processing.py
+++ src/run_data_processing.py
@@ -2,17 +2,13 @@ from tools.convert_data import convert_data
 from tools.process_data import process_data
 
 # variables
-it = 2
+it = 1
 offset = 4
 
 # directories
 fire_dir = "/Volumes/My Passport for Mac/m12i_res7100"
-# data_dir = "/Users/z5114326/Documents/GitHub/GC_kinematics/data/result/m12i"
-# snapshot_times_pub = "/Users/z5114326/Documents/GitHub/GC_kinematics/data/external/snapshot_times_public.txt"
-data_dir = "/Users/z5114326/Documents/GitHub/GC_kinematics_v2/data/result/m12i"
-snapshot_times_pub = (
-    "/Users/z5114326/Documents/GitHub/GC_kinematics_v2/data/external/snapshot_times_public.txt"
-)
+data_dir = "/Users/z5114326/Documents/GitHub/GC_kinematics/data/result/m12i"
+snapshot_times_pub = "/Users/z5114326/Documents/GitHub/GC_kinematics/data/external/snapshot_times_public.txt"
 
 # processing flags
 real_flag = 1  # (1 for just real, 0 for just not real, None for both)
